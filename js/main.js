edc42b28fed11aa11cd5a00fd9ea9c69dec447dc
pseudonam-gc <pseudonam1747@gmail.com>
2026-01-28 14:30:35 -0600

    Absolute path for sw.js

--- js/main.js
+++ js/main.js
@@ -376,13 +376,11 @@ class ThreesApp {
 
     registerServiceWorker() {
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('sw.js')
-                .then(registration => {
-                    console.log('SW registered:', registration.scope);
-                })
-                .catch(error => {
-                    console.log('SW registration failed:', error);
-                });
+  			const swPath = '/threes-web/sw.js'; // include repo name for project pages
+  			navigator.serviceWorker.register(swPath)
+    		.then(reg => console.log('SW registered:', reg))
+    		.catch(err => console.error('SW registration failed:', err));
+			}
         }
     }
 }
